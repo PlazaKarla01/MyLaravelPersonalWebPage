d786e8a2c5466035c2d4e3c98e06b800a59a1fab
Karla Marie Plaza <101855682+PlazaKarla01@users.noreply.github.com>
2022-06-15 15:20:40 +0800

    Update head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,1 +1,11 @@
+<meta charset="utf-8">
 
+<meta name="description" content="">
+
+<meta name="Saquib"content="Blade">
+
+<title>Check our Layout</title>
+
+<!--load bootsrap from cdn-->
+
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
