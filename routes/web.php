7569aa64707858482e3a8c8a660975bb1bab827e
Karla Marie Plaza <101855682+PlazaKarla01@users.noreply.github.com>
2022-06-15 15:16:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,19 @@ Route::get('/dashboard', function () {
 })->middleware(['auth','verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function()
+
+{
+
+    return View::make('pages.home');
+
+});
+
+Route::get('/contact', function()
+
+{
+
+    return View::make('pages.contact');
+
+});
