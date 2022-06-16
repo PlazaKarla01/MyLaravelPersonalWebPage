ded29266042cb680240677673c7635fe502ac494
Karla Marie Plaza <101855682+PlazaKarla01@users.noreply.github.com>
2022-06-16 13:54:10 +0800

    Create career.blade.php

---
+++ resources/views/pages/career.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.default')
+
+@section('content')
+
+<style>
+
+.career .career-content .text-7{
+    margin-top: 10px;
+    font-size: 25px;
+    text-align: justify;
+    color: #000000;
+    font-family: 'Poppins', sans-serif;
+}
+
+footer{
+  background: #4A235A;
+  padding: 15px 23px;
+  color: #C39BD3;
+  text-align: center;
+}
+
+</style>
+
+<body>
+<section class="career" id="career">
+    <div class="max-width">
+            <h2 class="title">Career</h2>
+            <div class="career-content">
+                    <p class="text-7">I envision myself becoming more skilled, efficient, and professional in the next 6 years. I want to spend the next several years learning new skills, taking on demanding work, working on various projects, contributing as much as I can, and putting in the effort to see myself advance to the next level.
+                         I feel that my hard work enables me to be a better version of myself, and I wanted to perceive myself in a positive light. It might be an fashion designer, or a graphic designer.Honestly, I have no idea what will happen in the next six years because there is nothing particular I want to be and I don't picture myself in any sort of work/career today, but there are two jobs that I would like to have in the future.
+                        And I learnt to take it easy and be patient because It's alright to not know some things. And there’s no advantage to rushing through life and there are a lot of opportunities everywhere. Also remember to look at hardships as opportunities and challenges as gifts. 
+                    </p>
+            </div>
+        </div>
+    </section>
+
+@stop
