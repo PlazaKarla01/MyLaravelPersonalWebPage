6d36dbe5394c7c8418eb65d69b019fa55a574618
Karla Marie Plaza <101855682+PlazaKarla01@users.noreply.github.com>
2022-06-16 13:20:59 +0800

    added more pages

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,5 +26,37 @@ Route::get('/contact', function()
 {
 
     return View::make('pages.contact');
+
+});
+
+Route::get('/about', function()
+
+{
+
+    return View::make('pages.about');
+
+});
+
+Route::get('/cs202', function()
+
+{
+
+    return View::make('pages.cs202');
+
+});
+
+Route::get('/laravel', function()
+
+{
+
+    return View::make('pages.laravel');
+
+});
+
+Route::get('/career', function()
+
+{
+
+    return View::make('pages.career');
 
 });
