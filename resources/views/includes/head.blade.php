ba43ec51387ba62b51a01ff693929a28f76edf67
Karla Marie Plaza <101855682+PlazaKarla01@users.noreply.github.com>
2022-06-16 13:22:50 +0800

    Update head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,11 +1,16 @@
-<meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
 
-<meta name="description" content="">
+    <meta name="description" content="">
+    <meta name="Karla Marie Plaza" content="Blade">
+    <title>PLAZA-MY-LARAVEL-PERSONAL-WEBPAGE</title>
 
-<meta name="Saquib"content="Blade">
 
-<title>Check our Layout</title>
-
-<!--load bootsrap from cdn-->
 
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="css/style.css">
+
+</head>
